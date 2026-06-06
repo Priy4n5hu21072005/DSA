89efb9dc74fead0b6eb163c118d2796bd4b1bc09
Priy4n5hu21072005 <priyanshukumar08357@gmail.com>
2026-06-06 20:09:46 +0530

    Java Revision

--- JavaRevisionAndPractice/chapter1.java
+++ JavaRevisionAndPractice/chapter1.java
@@ -1,13 +1,23 @@
 package JavaRevisionAndPractice;
-
+// Method Overloading 
 public class chapter1 {
     int a ;
     int b;
+    int c;
+    // Static Variable 
+    String name ="Priyanshu Learning";
     public chapter1(int x, int y){
         this.a=x;
         this.b=y;
     }
+    // Parametrized Constructor
+    public chapter1(int x , int y , int z){
+        this.a=x;
+        this.b=y;
+        this.c=z;
+    }
     public int Addition(){
+        System.out.println(name);
         return a+b;
     }
     public int Subtraction(){
---
+++ JavaRevisionAndPractice/chapter2.java
@@ -0,0 +1,20 @@
+package JavaRevisionAndPractice;
+//Encapsulation
+public class chapter2 {
+    public class MBA{
+        void study(){
+            System.out.println("learning MBA");
+        }
+    }
+    // Overriding
+    class Financial extends MBA{
+        void study(){
+            System.out.println("good!");
+        }
+    }
+    public static void main(String[] args) {
+        chapter2 outer =new chapter2();
+        MBA obj = outer.new Financial();
+        obj.study();
+    }
+}
