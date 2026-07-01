8648c215c0aff6fb2b40d54ed24e1bc90eed5059
Priy4n5hu21072005 <priyanshukumar08357@gmail.com>
2026-07-01 20:01:59 +0530

    Linked List array representation

--- Data-Structures/Stack/ArrayRepresentationAndOperations/ArrayRepresentationOfStack.py
+++ Data-Structures/Stack/ArrayRepresentationAndOperations/ArrayRepresentationOfStack.py
@@ -1,3 +1,5 @@
+# Insert Operation
+# Time complexity is O(1)
 class ArrayRepresentationOfStack:
     def ArrayRepresntation(self,x):
         if self.top == self.capacity -1:
@@ -5,3 +7,13 @@ class ArrayRepresentationOfStack:
             return
         self.top +=1
         self.array[self.top]=x
+
+    # Pop Operation
+    # time complexity = O(1)
+    def pop(self):
+        if self.top==-1:
+            print("Stack is underflow")
+            return -1
+        values = self.arr[self.top]
+        self.top -=1
+        return values
