2a44e55a60d75456bcfe0fe69123ec3e17481c45
Priy4n5hu21072005 <priyanshukumar08357@gmail.com>
2026-08-09 18:27:00 +0530

    leetcode problem for level by order traversal

---
+++ Data-Structures/Tree/Problems/102.Level Order Traversal in Binary Tree.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,value=0):
+        self.value=value
+        self.left =None
+        self.right=None
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        q=deque([root])
+        ans =[]
+        while q:
+            level =[]
+            for _ in range(len(q)):
+                node=q.popleft()
+                level.append(node.value)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(level)
+
+        return ans  
+
+     
