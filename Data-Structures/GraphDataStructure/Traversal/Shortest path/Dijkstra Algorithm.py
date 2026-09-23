a0bdc8c3d359d7ead14ebc03c3eeaffdf7b85370
Priy4n5hu21072005 <priyanshukumar08357@gmail.com>
2026-09-23 12:01:33 +0530

    graph day 12

---
+++ Data-Structures/GraphDataStructure/Traversal/Shortest path/Dijkstra Algorithm.py
@@ -0,0 +1,16 @@
+from collections import heapq
+def dijkstra(adj,src):
+    V=len(adj)
+    pq=[]
+    distance=[float("-inf")]*V
+    heapq.heappush(pq,(0,src))
+    while pq:
+        d,u=heapq.heappop(pq)
+        if d>distance[u]:
+            continue
+        for v,w in adj[u]:
+            if distance[u]+w>distance[v]:
+                distance[v]=distance[u]+w
+                heapq.heappush(pq,(distance[v],v))
+    return distance
+
